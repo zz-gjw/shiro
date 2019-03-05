354912dc5670cc6cd5a512d2a7fa68034b74c3f7
zz-gjw <18838226680@189.cn>
2019-03-05 11:55:16 +0800

    Shiro安全框架后台

---
+++ src/main/java/com/zz/shiro/config/ShiroConfig.java
@@ -0,0 +1,9 @@
+package com.zz.shiro.config;
+
+/**
+ * @Author : zz-gjw
+ * @Date : 2019/3/5 9:56
+ * @Description:
+ */
+public class ShiroConfig {
+}
---
+++ src/main/java/com/zz/shiro/shiro/UserRealm.java
@@ -0,0 +1,25 @@
+package com.zz.shiro.shiro;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+
+/**
+ * @Author : zz-gjw
+ * @Date : 2019/3/5 9:58
+ * @Description:
+ */
+public class UserRealm extends AuthorizingRealm {
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
+        return null;
+    }
+
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+        return null;
+    }
+}
